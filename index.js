120a2d59a75e5c4518e83af053de5c2ba6f120fc
amandaymxs <amandaxinvestment@gmail.com>
2020-12-31 16:15:32 -0500

    modified json object, success retreiving json data from ejs

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const data = require('./data.json');
 const port = 4000;
+const path = require('path');
 
 app.use(express.json());
 app.set('view engine', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+app.get('/posts', (req, res) => {
+    res.render('posts/index', { ...data });
+})
 app.listen(port, (req, res) => {
     console.log("Listening on port 4000.");
 })
