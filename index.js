ebdc81742f0e4a7f27a61b51f176f2653da493ab
amandaymxs <amandaxinvestment@gmail.com>
2021-01-03 21:04:08 -0500

    modified ids, addEventListener to new.ejs form to verified required fields filled

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const data = require('./data.json');
 
 const port = 4000;
 const path = require('path');
+const { type } = require('jquery');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -16,9 +18,14 @@ app.get('/posts/new', (req, res) => {
     res.render('posts/new');
 })
 
+app.post('/posts', (req, res) => {
+    const { name, age, color, breed, gender, backstory, source, sourceType, adoptionFee, feeIncludes } = req.body;
+    posts.unshift({ name, age, color, breed, gender, backstory, source, sourceType, adoptionFee, feeIncludes });
+    res.send("It worked");
+})
 //Read
 app.get('/posts', (req, res) => {
-    res.render('posts/index', { ...data });
+    res.render('posts/index', { data });
 })
 
 
--- public/js/assets/create.js
+++ public/js/assets/create.js
@@ -6,11 +6,31 @@ let otherInput = document.querySelector('.otherInput');
 other.addEventListener('click', () => {
     if (other.checked) {
         otherInput.style.display = 'inline';
+        otherInput.setAttribute("required", "");
         console.log("show");
     } else {
         otherInput.style.display = 'none';
+        otherInput.removeAttribute("required");
         console.log("hide");
     }
-    // (other.checked ? (otherInput.style.visibility = "show") && console.log("Show")
-    //  : (otherInput.style.visibility = "hidden") && console.log("Hide"));
 })
+
+document.getElementById('submit').onclick = () => {
+    let allRequiredFilled = true;
+    document.getElementById('adoptionForm').querySelectorAll("[required]").forEach(function (answer) {
+        if (!allRequiredFilled) {
+            return;
+        }
+
+        if (!answer.value) {
+            allRequiredFilled = false;
+        }
+
+        if ((age.value == "") || (gender.value == "")) {
+            allRequiredFilled = false;
+        }
+    })
+    if (!allRequiredFilled) {
+        alert('Information is missing, please review form.')
+    }
+}
