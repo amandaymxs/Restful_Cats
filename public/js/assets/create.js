33b30cbf2788996577bd5a90a5c828d515f887a1
amandaymxs <amandaxinvestment@gmail.com>
2021-01-05 00:38:07 -0500

    clickOne clickTwo added into js

--- public/js/assets/create.js
+++ public/js/assets/create.js
@@ -8,28 +8,45 @@ other.addEventListener('click', () => {
     if (other.checked) {
         otherInputSpan.style.display = 'inline';
         otherInput.setAttribute("required", "");
-        console.log("show");
     } else {
         otherInputSpan.style.display = 'none';
         otherInput.removeAttribute("required");
-        console.log("hide");
+    }
+})
+let clickOne;
+let clickTwo;
+
+document.addEventListener('click', (e) => {
+    if ((!clickOne) && (e.target.id != "")) {
+        clickOne = e.target;
+    } else {
+        clickTwo = clickOne;
+        clickOne = e.target;
+        if (clickOne.id != clickTwo.id) {
+            console.log(clickTwo.value);
+            console.dir(clickTwo);
+            // }
+        }
+
     }
 })
 
-document.getElementById('submit').onclick = () => {
-    let allRequiredFilled = true;
-    document.getElementById('adoptionForm').querySelectorAll("[required]").forEach(function (answer) {
-        if (!allRequiredFilled) {
-            return;
-        }
 
-        if ((!answer.value) || (age.value == "") || (gender.value == "") || (otherInput.value == "")) {
-            allRequiredFilled = false;
-        }
-    })
-    if (!allRequiredFilled) {
-        alert('Information is missing, please review form.')
-    } else {
-        alert('Completed');
-    }
-}
+// document.getElementById('submit').onclick = () => {
+//     let allRequiredFilled = true;
+//     document.getElementById('adoptionForm').querySelectorAll("[required]").forEach(function (answer) {
+//         if (!allRequiredFilled) {
+//             return;
+//         }
+
+//         if ((!answer.value) || (age.value == "") || (gender.value == "") || (otherInput.value == "")) {
+//             allRequiredFilled = false;
+//         }
+//     });
+//     if (!allRequiredFilled) {
+//         alert('Information is missing, please review form.');
+//         return;
+//     } else {
+//         alert('Submitted');
+//     }
+// }
