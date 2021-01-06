3b1a77baefc276e38a53ce3c5fe7b4ac0c8ce16e
amandaymxs <amandaxinvestment@gmail.com>
2021-01-06 17:52:03 -0500

    textarea counter

--- public/js/assets/create.js
+++ public/js/assets/create.js
@@ -17,36 +17,45 @@ let clickOne;
 let clickTwo;
 
 document.addEventListener('click', (e) => {
-    if ((!clickOne) && (e.target.id != "")) {
+    if ((!clickOne) && (e.target.id != "") && (e.target.type == "text" | "backStory")) {
         clickOne = e.target;
-    } else {
+    } else if ((clickOne) && (e.target.id != "") && (e.target.type == "text")) {
         clickTwo = clickOne;
         clickOne = e.target;
         if (clickOne.id != clickTwo.id) {
-            console.log(clickTwo.value);
-            console.dir(clickTwo);
-            // }
+            console.log(clickOne.id, clickTwo.id);
+            if (clickTwo.value.length < 4) {
+                console.log("Error", clickTwo.value.length);
+            }
+        } else {
+
         }
 
     }
 })
 
+let backStory = document.getElementById('backStory');
 
-// document.getElementById('submit').onclick = () => {
-//     let allRequiredFilled = true;
-//     document.getElementById('adoptionForm').querySelectorAll("[required]").forEach(function (answer) {
-//         if (!allRequiredFilled) {
-//             return;
-//         }
+backStory.addEventListener('input', (e) => {
+    let feedbackCounter = document.getElementsByClassName('counter');
+    const target = e.currentTarget;
+    const maxLength = target.getAttribute("maxlength");
+    const currentLength = target.value.length;
+    feedbackCounter[0].innerText = maxLength - currentLength;
+})
 
-//         if ((!answer.value) || (age.value == "") || (gender.value == "") || (otherInput.value == "")) {
-//             allRequiredFilled = false;
-//         }
-//     });
-//     if (!allRequiredFilled) {
-//         alert('Information is missing, please review form.');
-//         return;
-//     } else {
-//         alert('Submitted');
-//     }
-// }
+document.getElementById('submit').addEventListener('', () => {
+    let allRequiredFilled = true;
+    document.getElementById('adoptionForm').querySelectorAll("[required]").forEach(function (answer) {
+        if (!allRequiredFilled) {
+            return;
+        } else if ((!answer.value) || (age.value == "") || (gender.value == "") || (otherInput.value == "")) {
+            allRequiredFilled = false;
+        }
+    });
+    if (allRequiredFilled) {
+        console.log('removed')
+        document.getElementById('submit').removeAttr('disabled');
+    }
+})
+
