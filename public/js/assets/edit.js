953027513b635f9386ab3cc9a6ba39a6d9ac30e3
amandaymxs <amandaxinvestment@gmail.com>
2021-01-18 18:10:29 -0500

    edit.js attempt to manipulate options -> selected

--- public/js/assets/edit.js
+++ public/js/assets/edit.js
@@ -1,7 +1,13 @@
-function ifSelected(item) {
+function ifSelected(list) {
     // let feeIncludesItem = document.querySelectorAll(".feeIncludesItem");
-    console.log(item);
-
+    // let string = item.replaceAll(",", ", ");
+    // list = list.replaceAll(" ", "");
+    let itemArray = list.split(",");
+    for (let item in itemArray) {
+        console.log(itemArray[item]);
+        // let hello = document.querySelector('#feeIncludes').options.;
+        // hello.setAttribute('selected', 'selected');
+    }
     // if (feeIncludesItems.includes(item)) {
     //     let itemId = item.replace(' ', '');
     //     document.querySelector(`#${itemId}`).setAttribute('selected', 'selected');
