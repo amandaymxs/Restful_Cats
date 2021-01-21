37a9c3eb645a173f9c8074109e764f74fb387153
amandaymxs <amandaxinvestment@gmail.com>
2021-01-20 22:12:16 -0500

    removal of feedback in create.js, added buttons in forms

--- public/js/assets/create.js
+++ public/js/assets/create.js
@@ -1,53 +1,11 @@
-let other = document.querySelector('#other');
-let otherInputSpan = document.querySelector('.otherInputSpan');
-let otherInput = document.querySelector('.otherInput');
-
-let clickOne;
-let clickTwo;
-let feedbackNum;
-
-let allInputText = document.getElementsByClassName('userInputText');
-let allFeedback = document.getElementsByClassName('feedback');
-
-document.addEventListener('click', (e) => {
-    //press tab does not verify information
-    if ((!clickOne) && (e.target.id != "") && (e.target.type == "text")) {
-        clickOne = e.target;
-    } else if ((clickOne) && (e.target.id != "") && (e.target.type == "text")) {
-        clickTwo = clickOne;
-        clickOne = e.target;
-        if (clickOne.id != clickTwo.id) {
-            let success = '\u2713';
-            let fail = '\u2717';
-            let num = clickTwo.id.replace(/\D/g, "");
-            let fbN = 'feedback' + num;
-            let feedbackNum = document.querySelector('#' + fbN);
-            if (clickTwo.value.length > 3) {
-                feedbackNum.innerText = success;
-                feedbackNum.style.color = 'green';
-            } else {
-                feedbackNum.innerText = fail;
-                feedbackNum.style.color = 'red';
-            }
-        }
-    }
-})
-
 let backStory = document.getElementById('backStory');
+let allFeedback = document.getElementsByClassName('feedback');
+let feedbackCounter = document.getElementById('counter');
+
 
 backStory.addEventListener('input', (e) => {
-    let feedbackCounter = document.getElementById('counter');
     const target = e.currentTarget;
     const maxLength = target.getAttribute("maxlength");
     const currentLength = target.value.length;
     feedbackCounter.innerText = maxLength - currentLength;
 })
-
-function validateForm(e) {
-    try {
-        error
-    } catch (err) {
-        console.log(err);
-        e.preventDefault();
-    }
-}
--- public/js/assets/edit.js
+++ public/js/assets/edit.js
@@ -1,9 +1,9 @@
-function ifSelected(list) {
+function isSelected(list) {
     let listArray = list.split(",");
     let feeIncludes = document.getElementById('feeIncludes');
     for (let element in feeIncludes) {
         for (let item of listArray) {
-            if (feeIncludes[element].getAttribute('value').toUpperCase() == item.toUpperCase()) {
+            if (feeIncludes[element].value == item) {
                 feeIncludes[element].setAttribute('selected', 'selected');
             }
         }
